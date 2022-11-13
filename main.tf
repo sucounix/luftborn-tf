9f4a440e13c41de76adf49c8d32979f9cb6e94d9
Ahmed Ali <sucounix@gmail.com>
2022-11-13 20:45:10 +0200

    Init.

---
+++ main.tf
@@ -0,0 +1,169 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.93.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id = ""
+  client_id       = ""
+  client_secret   = ""
+  tenant_id       = ""
+  features {}
+}
+
+
+locals {
+  resource_group="app-grp"
+  location="North Europe"
+}
+
+
+resource "tls_private_key" "linux_key" {
+  algorithm = "RSA"
+  rsa_bits = 4096
+}
+
+# We want to save the private key to our machine
+# We can then use this key to connect to our Linux VM
+
+resource "local_file" "linuxkey" {
+  filename="linuxkey.pem"  
+  content=tls_private_key.linux_key.private_key_pem 
+}
+
+# Create a resource group for network
+resource "azurerm_resource_group" "network-rg"{
+  name=local.resource_group
+  location=local.location
+}
+
+# Create the network VNET
+resource "azurerm_virtual_network" "network-vnet" {
+  name                = "app-network-vnet"
+  address_space       = ["10.0.0.0/16"]
+  resource_group_name = azurerm_resource_group.network-rg.name
+  location            = azurerm_resource_group.network-rg.location
+}
+
+# Create a subnet for VM
+resource "azurerm_subnet" "vm-subnet" {
+  name                 = "app-vm-subnet"
+  address_prefixes     = ["10.0.1.0/24"]
+  virtual_network_name = azurerm_virtual_network.network-vnet.name
+  resource_group_name  = azurerm_resource_group.network-rg.name
+}
+
+# Create Network Security Group
+resource "azurerm_network_security_group" "nginx-vm-nsg" {
+  depends_on=[azurerm_resource_group.network-rg]
+
+  name                = "nginx-nsg"
+  location            = azurerm_resource_group.network-rg.location
+  resource_group_name = azurerm_resource_group.network-rg.name
+
+  security_rule {
+    name                       = "Allow-SSH"
+    description                = "Allow SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "Internet"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-HTTP"
+    description                = "Allow HTTP"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "Internet"
+    destination_address_prefix = "*"
+  }
+
+}
+
+# Associate the web NSG with the subnet
+resource "azurerm_subnet_network_security_group_association" "nginx-vm-nsg-association" {
+  depends_on=[azurerm_resource_group.network-rg]
+
+  subnet_id                 = azurerm_subnet.vm-subnet.id
+  network_security_group_id = azurerm_network_security_group.nginx-vm-nsg.id
+}
+
+# Get a Static Public IP
+resource "azurerm_public_ip" "nginx-vm-ip" {
+  depends_on=[azurerm_resource_group.network-rg]
+
+  name                = "nginx-pub-ip"
+  location            = azurerm_resource_group.network-rg.location
+  resource_group_name = azurerm_resource_group.network-rg.name
+  allocation_method   = "Static"
+
+}
+
+# Create Network Card for the VM
+resource "azurerm_network_interface" "nginx-nic" {
+  depends_on=[azurerm_resource_group.network-rg]
+
+  name                = "nginx-if-nic"
+  location            = azurerm_resource_group.network-rg.location
+  resource_group_name = azurerm_resource_group.network-rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.vm-subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.nginx-vm-ip.id
+
+   }
+}
+
+# Bootstrapping Template File
+data "template_file" "nginx-vm-cloud-init" {
+  template = file("install-nginx.sh")
+}
+
+# Create Nginx VM
+resource "azurerm_linux_virtual_machine" "linux_vm" {
+  name                = "linuxvm"
+  resource_group_name = local.resource_group
+  location            = local.location
+  size                = "Standard_D2s_v3"
+  admin_username      = "linuxusr"  
+  network_interface_ids = [
+    azurerm_network_interface.nginx-nic.id,
+  ]
+  admin_ssh_key {
+    username   = "linuxusr"
+    public_key = tls_private_key.linux_key.public_key_openssh
+  }
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+  custom_data    = base64encode(data.template_file.nginx-vm-cloud-init.rendered)
+
+  depends_on = [
+    azurerm_network_interface.nginx-nic,
+    tls_private_key.linux_key
+  ]
+}
+
